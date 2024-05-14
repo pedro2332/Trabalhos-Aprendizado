329b52a27d189e0161c33cca32332167939f23bb
pedro2332 <115152065+pedro2332@users.noreply.github.com>
2024-05-14 11:42:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let listaGenerica = [];
+let listaLinguagensDeProgramação = ['Java', 'Ruby', 'GoLang'];
+console.log(listaLinguagensDeProgramação[0]);
+console.log(listaLinguagensDeProgramação[1]);
+console.log(listaLinguagensDeProgramação[2]);
